347d9f86c1ff5f59d0e94ce9d2818740a3901d63
Marçal <edumsfilho347@gmail.com>
2023-05-17 14:37:55 -0300

    configurando cenario mock para testes usando repository

--- projeto/src/test/java/com/iftm/client/services/ClientServiceTest.java
+++ projeto/src/test/java/com/iftm/client/services/ClientServiceTest.java
@@ -1,4 +1,21 @@
 package com.iftm.client.services;
 
+import org.aspectj.apache.bcel.Repository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 public class ClientServiceTest {
+    @InjectMocks
+    ClientService service;
+
+    @Mock
+    Repository repository;
+
+    
+
+
 }
