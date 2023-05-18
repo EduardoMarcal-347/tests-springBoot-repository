7cee5638ea141770b363e5ecf52cf55d37b9531e
Marçal <edumsfilho347@gmail.com>
2023-05-18 11:22:36 -0300

    adicionando teste que testa o metodo findAll(PageRequest pageRequest)

--- projeto/src/test/java/com/iftm/client/services/ClientServiceTest.java
+++ projeto/src/test/java/com/iftm/client/services/ClientServiceTest.java
@@ -1,5 +1,7 @@
 package com.iftm.client.services;
 
+import com.iftm.client.dto.ClientDTO;
+import com.iftm.client.entities.Client;
 import com.iftm.client.repositories.ClientRepository;
 import com.iftm.client.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -10,8 +12,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.times;
@@ -26,7 +35,7 @@ public class ClientServiceTest {
 
     @Test
     @DisplayName("Testa metodo delete quando o id existir")
-    public void testaDeleteIdExistente(){
+    public void testeDeleteIdExistente(){
         Long id = 1L;
 
         service.delete(id);
@@ -36,7 +45,7 @@ public class ClientServiceTest {
 
     @Test
     @DisplayName("Testa metodo delete quando o id não existir")
-    public void testaDeleteIdInexistente(){
+    public void testeDeleteIdInexistente(){
         Long id = 129329323L;
 
         Mockito.doThrow(ResourceNotFoundException.class).when(repository).deleteById(id);
@@ -44,5 +53,25 @@ public class ClientServiceTest {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> service.delete(id));
     }
 
+    @Test
+    @DisplayName("Testa Metodo findAllPaged que deve retornar uma página com todos os clientes")
+    public void testeFindAllPaged(){
+        List<Client> clientes = new ArrayList<>(Arrays.asList(
+                new Client(1L,"Joao", "123", 730D, Instant.now(), 3),
+                new Client(2L,"Maria", "321", 1014D, Instant.now(), 1),
+                new Client(3L,"Fulana", "007", 2030D, Instant.now(), 0)
+        ));
+        PageRequest pageRequest = PageRequest.of(0, clientes.size());
+        Page<Client> page = new PageImpl<>(clientes);
+
+        Mockito.when(repository.findAll(pageRequest)).thenReturn(page);
+        Page<ClientDTO> resultado = service.findAllPaged(pageRequest);
+
+        Assertions.assertEquals(clientes.size(), resultado.getSize());
+        Assertions.assertEquals(clientes.get(0).getId(), resultado.getContent().get(0).getId());
+        Assertions.assertEquals(clientes.get(1).getId(), resultado.getContent().get(1).getId());
+        Assertions.assertEquals(clientes.get(2).getId(), resultado.getContent().get(2).getId());
+        
+    }
 
 }
